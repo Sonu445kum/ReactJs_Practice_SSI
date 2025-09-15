6276f798d55f74ea2ef7ae1ee63968444f0d55dd
Sonu445Kum <sonuroy1629@gmail.com>
2025-09-15 18:12:06 +0530

    Create a ToggleButton For Change Text Hello To Goodbye

--- src/Components/BasicOperations.jsx
+++ src/Components/BasicOperations.jsx
@@ -6,6 +6,9 @@ export default function BasicOperations() {
     //create a useState for initialize the initial State
     const [count,setCount] =useState(0);
 
+    // create a useState For Toggle Text that is Hello->GoodBye->Hello
+    const [toggle,setToggle] =useState("Hello");
+
     // Create handleIncrement functions
     const  handleIncrement=()=>{
         setCount(count+1);
@@ -19,6 +22,10 @@ export default function BasicOperations() {
         setCount(0);
     }
 
+    const handleToggleText=()=>{
+        // Here we are using the ternary operators
+        setToggle((prevText)=>prevText==="Hello"? "GoodBye":"Hello")
+    }
   return (
     <div>
         {/* Simple Message  */}
@@ -31,6 +38,13 @@ export default function BasicOperations() {
         <button onClick={handleDecrement}>Decrement:</button>
         {/* Reset Button */}
         <button onClick={handleReset}>Reset:</button>
+
+        {/* Toggle Text
+        Create a button that toggles text between "Hello" and "Goodbye" when clicked. 
+        */}
+        <h3>Toggle Text:</h3>
+        <button onClick={handleToggleText} >ToggleText</button>
+        <h1>{toggle}</h1>
     
     </div>
   )
